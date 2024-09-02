4f082dc2d32ce6746c88e7cd1a284067ddb2ecd3
heratok <goku55673@gmail.com>
2024-09-01 21:12:03 -0500

    arreglando errores para que pueda compilar la apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // Importa flutter_web_plugins
+// import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // Importa flutter_web_plugins
 import 'package:ingeconomica/routes/routes.dart';
 
 void main() {
   // Configura Flutter para usar la estrategia de URL sin hash.
-  setUrlStrategy(PathUrlStrategy());
+  // setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
