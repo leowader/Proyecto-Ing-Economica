9ab1d3592c42352993a510fc631cb80506638ff7
heratok <goku55673@gmail.com>
2024-10-21 13:02:05 -0500

    añadiendo huella en pagos

--- lib/screens/pagos/pagos.dart
+++ lib/screens/pagos/pagos.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart'; // Importa para autenticación biométrica
+import 'package:flutter_local_notifications/flutter_local_notifications.dart'; // Importa para notificaciones
 
 class Pagos extends StatefulWidget {
   final double saldoDisponible;
@@ -14,21 +16,67 @@ class _PagosState extends State<Pagos> {
   final _formKey = GlobalKey<FormState>();
   double cuota = 0.0;
   String descripcion = '';
+  final LocalAuthentication auth = LocalAuthentication();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-  void _pagarCuota() {
+  @override
+  void initState() {
+    super.initState();
+    _initializeNotifications();
+  }
+
+  void _initializeNotifications() {
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> _mostrarNotificacion(double cantidad) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Pago realizado',
+      'Has pagado \$${cantidad.toStringAsFixed(2)}',
+      platformChannelSpecifics,
+    );
+  }
+
+  Future<void> _pagarCuota() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
       if (cuota <= widget.saldoDisponible) {
-        // Realiza el pago y notifica a la HomeScreen
-        widget.onPagoRealizado(cuota, descripcion);
+        // Realiza la autenticación biométrica
+        final bool isAuthenticated = await auth.authenticate(
+          localizedReason: 'Confirma tu identidad para realizar el pago',
+          options: const AuthenticationOptions(biometricOnly: true),
+        );
 
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Pago exitoso.'),
-        ));
+        if (isAuthenticated) {
+          // Realiza el pago y notifica a la HomeScreen
+          widget.onPagoRealizado(cuota, descripcion);
 
-        // Regresar a la HomeScreen
-        Navigator.pop(context);
+          // Muestra la notificación
+          _mostrarNotificacion(cuota);
+
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Pago exitoso.'),
+          ));
+
+          // Regresar a la HomeScreen
+          Navigator.pop(context);
+        } else {
+          // Manejar caso de fallo en la autenticación
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Autenticación fallida')),
+          );
+        }
       } else {
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
           content: Text('Saldo insuficiente para pagar la cuota.'),
@@ -40,7 +88,7 @@ class _PagosState extends State<Pagos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Pagar Cuota')),
+      appBar: AppBar(title: const Text('Pagar Cuota')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -49,11 +97,12 @@ class _PagosState extends State<Pagos> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                  'Saldo disponible: \$${widget.saldoDisponible.toStringAsFixed(2)}',
-                  style: TextStyle(fontSize: 24)),
-              SizedBox(height: 20),
+                'Saldo disponible: \$${widget.saldoDisponible.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Cuota a pagar'),
+                decoration: const InputDecoration(labelText: 'Cuota a pagar'),
                 keyboardType: TextInputType.number,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -68,14 +117,14 @@ class _PagosState extends State<Pagos> {
                   cuota = double.parse(value!);
                 },
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Descripción del pago'),
+                decoration: const InputDecoration(labelText: 'Descripción del pago'),
                 onSaved: (value) {
                   descripcion = value ?? '';
                 },
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _pagarCuota,
                 style: ElevatedButton.styleFrom(
